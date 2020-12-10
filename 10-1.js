cacd3c7c257aaea1d571c400c1731d5b29839d9f
Adam Kiss <iam@adamkiss.com>
2020-12-10 18:51:35 +0100

    day 10 p1; clean code to prepare for p2

--- 10-1.js
+++ 10-1.js
@@ -1,12 +1,14 @@
 const adapters = require(true ? './inputs/10-input' : './10-0-test')
   .split("\n").map(i => parseInt(i, 10)).sort((a,b) => a - b)
 const outlet = 0
-const device = input[input.length-1] + 3
+const device = adapters[adapters.length-1] + 3
 
 const diffs = [outlet, ...adapters, device]
-  .reduce((acc, val, i, arr) => {
-    if (i === 0) return acc
-    acc[arr[i] - arr[i - 1]] += 1
-    return acc
-  }, [0,0,0,0])
-console.log(diffs[1]*diffs[3]);
+  .reduce((r, v, i, a) => (i === 0) ? r : [...r, v - a[i-1]], [])
+
+const diffCounts = diffs.reduce((r, v) => {
+  r[v] += 1
+  return r
+}, [0,0,0,0])
+
+console.log(diffCounts[1]*diffCounts[3]);
