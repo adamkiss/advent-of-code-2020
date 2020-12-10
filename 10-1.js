05063e739619742f20a304c1e1d42d51746d6c51
Adam Kiss <iam@adamkiss.com>
2020-12-10 18:38:35 +0100

    reanme values around so I remember stuff

--- 10-1.js
+++ 10-1.js
@@ -1,7 +1,9 @@
-const input = require(true ? './inputs/10-input' : './10-0-test')
+const adapters = require(true ? './inputs/10-input' : './10-0-test')
   .split("\n").map(i => parseInt(i, 10)).sort((a,b) => a - b)
+const outlet = 0
+const device = input[input.length-1] + 3
 
-const diffs = [0, ...input, input[input.length-1] + 3]
+const diffs = [outlet, ...adapters, device]
   .reduce((acc, val, i, arr) => {
     if (i === 0) return acc
     acc[arr[i] - arr[i - 1]] += 1
