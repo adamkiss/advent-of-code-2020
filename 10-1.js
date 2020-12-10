b29bce456c86e75209b68689872521b46f04d141
Adam Kiss <iam@adamkiss.com>
2020-12-10 19:40:38 +0100

    10: Final p1

--- 10-1.js
+++ 10-1.js
@@ -1,13 +1,11 @@
 const data = {t1: './10-0-test2', t2: './10-0-test', r: './inputs/10-input'}
-const adapters = require(data.t2)
+const adapters = require(data.r)
   .split("\n").map(i => parseInt(i, 10)).sort((a,b) => a - b)
 const outlet = 0
 const device = adapters[adapters.length-1] + 3
 
 const diffs = [outlet, ...adapters, device]
-  .reduce((r, v, i, a) => (i === 0) ? r : [...r, v - a[i-1]], [])
-
-console.log([outlet, ...adapters, device].map(i => ` ${(' '+i).slice(-2)}`).join(''))
+  .reduce((r, v, i, a) => (i === 0) ? [0] : [...r, v - a[i-1]], [])
 
 const diffCounts = diffs.reduce((r, v) => {
   r[v] += 1
