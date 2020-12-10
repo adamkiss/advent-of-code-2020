bf806ee229db1a258ed6e678afaba14b06397b98
Adam Kiss <iam@adamkiss.com>
2020-12-10 19:01:07 +0100

    Log bigger test

--- 10-1.js
+++ 10-1.js
@@ -1,11 +1,13 @@
-const data = {t1: './10-0-test2', t2: './10-0-test2', r: './inputs/10-input'}
-const adapters = require(data.t1)
+const data = {t1: './10-0-test2', t2: './10-0-test', r: './inputs/10-input'}
+const adapters = require(data.t2)
   .split("\n").map(i => parseInt(i, 10)).sort((a,b) => a - b)
 const outlet = 0
 const device = adapters[adapters.length-1] + 3
 
 const diffs = [outlet, ...adapters, device]
   .reduce((r, v, i, a) => (i === 0) ? r : [...r, v - a[i-1]], [])
+
+console.log([outlet, ...adapters, device].map(i => ` ${(' '+i).slice(-2)}`).join(''))
 
 const diffCounts = diffs.reduce((r, v) => {
   r[v] += 1
