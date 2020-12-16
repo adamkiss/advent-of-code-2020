221c98e543aa2ec41927ef674854c880ef998530
Adam Kiss <iam@adamkiss.com>
2020-12-16 12:30:42 +0100

    day 16 part 2 done

--- 16-2.js
+++ 16-2.js
@@ -1,4 +1,4 @@
-const {rules, nearby} = require(true ? './inputs/16-input' : './16-0-test2')
+const {rules, ticket, nearby} = require(true ? './inputs/16-input' : './16-0-test2')
 
 const tickets = nearby.split("\n").map(ticket => ticket.split(',').map(i => parseInt(i, 10)))
 const fieldRules = rules.split("\n").map(rule => {
@@ -11,47 +11,26 @@ const fieldRules = rules.split("\n").map(rule => {
 })
 
 const isFieldValidForRange = (field, [[min1, max1], [min2, max2]]) => (field >= min1 && field <= max1) || (field >= min2 && field <= max2)
-const isFieldValid = field => {
-  return fieldRules
-    .map(rule => isFieldValidForRange(field, rule.ranges))
-    .filter(i => i)
-    .length > 0
-}
 
-const validTickets = tickets.filter(ticket => ticket.filter(field => isFieldValid(field)).length === ticket.length)
-validTickets
-  .map(ticket => {
-    const fields = ticket
-    return fields.map((field, index) => {
-      const impossible = fieldRules.map(r => isFieldValidForRange(field, r.ranges) ? null : r.name).filter(n => n)
-      return impossible.length > 0 ? impossible.map(impossibleField => ({field: impossibleField, index})) : null
-    }).filter(impossible => impossible !== null).flat()
-  })
-  .flat()
-  .forEach(({field, index}) => {
-    console.log(field, index)
-    const whichRule = fieldRules.findIndex(({name}) => name === field)
-    const indexInPossible = fieldRules[whichRule].possibleIndex.findIndex(i => i === index)
-    if (indexInPossible === -1) return
+const validTickets = tickets.filter(fields => fields.every(field => fieldRules.some(rule => isFieldValidForRange(field, rule.ranges))))
+const possibleFields = Array(validTickets[0].length).fill()
+  .map((_, i) => ({
+    i,
+    possible: fieldRules.filter(({ranges}) => validTickets.every(t => isFieldValidForRange(t[i], ranges))).map(r => r.name)
+  }))
+  .sort((a,b) => a.possible.length - b.possible.length)
 
-    fieldRules[whichRule].possibleIndex.splice(indexInPossible, 1)
-  })
+const fields = possibleFields.reduce((done, current) => [...done, {
+  i: current.i,
+  field: current.possible.filter(pf => done.every(d => pf !== d.field))[0]
+}], [])
 
-console.log(fieldRules)
+console.log(fields)
 
-const fieldRulesWithMoreIndicesPossible = () => fieldRules.map(({possibleIndex}) => possibleIndex.length).filter(i => i > 1).length
-
-let unsureIndexCount = fieldRulesWithMoreIndicesPossible()
-while(unsureIndexCount > 0) {
-  const definiteIndices = fieldRules.map(({possibleIndex}) => possibleIndex.length === 1 ? possibleIndex[0] : null).filter(i => i)
-  definiteIndices.forEach(definiteIndex => {
-    fieldRules.forEach((rule, ruleIndex) => {
-      if (rule.possibleIndex.length === 1) return
-      if (!rule.possibleIndex.includes(definiteIndex)) return
-      const indexToRemove = rule.possibleIndex.findIndex(i => i === definiteIndex)
-      fieldRules[ruleIndex].possibleIndex.splice(indexToRemove, 1)
-    })
-  });
-  unsureIndexCount = fieldRulesWithMoreIndicesPossible()
-  console.log(fieldRules)
-}
+const your = ticket.split(',').map(i => parseInt(i, 10))
+console.log(your, fields.map(f => `[${f.i}] ${f.field}: ${your[f.i]}`))
+console.log(
+  fields
+    .map(({field, i}) => /^departure/.test(field) ? your[i] : 1)
+    .reduce((sum, c) => sum *= c, 1)
+)
